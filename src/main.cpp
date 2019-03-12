cb1556908d18d00b5b969dceb582efc508822741
songpeng <song.peng@bizerba.com>
2019-03-12 14:13:00 +0800

    'success_to_run_with_empty_window_in_qml'

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,25 @@
 
 //For static link
 #ifdef QT_STATIC
+    //Qt
     #include <QtCore/QtPlugin>
     //QWindowsIntegrationPlugin "platforms/qwindows.dll" //Qt5Gui_QWindowsIntegrationPlugin.cmake
     Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
+    //Install Qml plugins
+#include <QtQml/QQmlExtensionPlugin>
+    //${QQMLDIR}QtQuick.2/qtquick2plugind.lib
+    Q_IMPORT_PLUGIN(QtQuick2Plugin)
+    //${QQMLDIR}QtQuick/Window.2/windowplugind.lib
+    Q_IMPORT_PLUGIN(QtQuick2WindowPlugin)
 #endif
 
 int main(int argc, char *argv[]) {
-/**/
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
+    return app.exec();
 }
 
 
